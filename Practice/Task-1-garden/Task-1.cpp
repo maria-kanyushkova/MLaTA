ce7962128d94a075a5b870ea8609f35f5ff97420
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-02-20 13:15:42 +0300

    добавлено решение задачи со второй практики про рынок

---
+++ Practice/Task-1-garden/Task-1.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+
+const unsigned int n = 201;
+const unsigned int m = 201;
+int arr[n][m];
+
+
+void volna(int i, int j) {
+	if (arr[i][j] == 1) {
+		arr[i][j] = 0;
+		volna(i + 1, j);
+		volna(i - 1, j);
+		volna(i, j + 1);
+		volna(i, j - 1);
+	}
+}
+
+int main()
+{
+	ifstream in("input3.txt"); 
+
+	int N = 0;
+	int M = 0;
+
+	int i = 0;
+	int j = 0;
+
+	char val = ' ';
+
+	in >> N >> M;
+
+	for (i = 0; i < N + 2; i++) {
+		arr[i][0] = 0;
+		arr[i][M + 2] = 0;
+	}
+
+	for (j = 0; i < M + 2; i++) {
+		arr[0][j] = 0;
+		arr[0][N + 2] = 0;
+	}
+
+	for (i = 1; i < N + 1; i++) {
+		for (j = 1; j < M + 1; j++) {
+			in >> val;
+			if (val == '#') {
+				arr[i][j] = 1;
+			}
+			if (val == '.') {
+				arr[i][j] = 0;
+			}
+		}
+	}
+
+	cout << endl;
+
+	int grad = 0;
+
+	for (i = 1; i < N + 1; i++) {
+		for (j = 1; j < M + 1; j++) {
+			if (arr[i][j] == 1) {
+				grad++;
+			}
+			volna(i, j);
+		}
+	}
+
+	cout << grad << endl;
+		
+	in.close();   
+
+	return 0;
+}
---
+++ Practice/Task-2-market/Task-2.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+long long maxCost = 0;
+long long totalMaxPrice = 0;
+
+vector<long long> tokenize(string line) {
+	stringstream str(line);
+	vector<long long> items;
+	string token;
+	while (getline(str, token, ' '))
+	{
+		items.emplace_back(stoi(token));
+	}
+
+	return items;
+}
+
+int main()
+{
+	ifstream input("input7.txt");
+	string line;
+
+	auto N = 0;
+	auto M = 0;
+	input >> N >> M;
+
+	getline(input, line);
+	line.clear();
+
+	vector<long long> sellers;
+	sellers.reserve(N);
+	getline(input, line);
+	sellers = tokenize(line);
+
+	vector<long long> consumers;
+	consumers.reserve(M);
+	getline(input, line);
+	consumers = tokenize(line);
+
+	sort(sellers.begin(), sellers.end());
+	sort(consumers.begin(), consumers.end());
+
+	auto sellersPosition = 0;
+	auto consumersPosition = 0;
+	auto sellerAgree = 0;
+	auto consumerAgree = 0;
+
+	for (size_t i = 0; i < consumers.size(); ++i)
+	{
+		const auto maxPrice = consumers[i];
+
+		for (size_t j = sellersPosition; j < sellers.size(); ++j)
+		{
+			const auto seller = sellers[j];
+			if (seller <= maxPrice)
+			{
+				sellersPosition++;
+				sellerAgree++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		for (size_t j = consumersPosition; j < consumers.size(); ++j)
+		{
+			const auto consumer = consumers[j];
+			if (consumer >= maxPrice)
+			{
+				consumersPosition++;
+				consumerAgree++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		const auto minPeople = min(sellerAgree, (int)(consumerAgree - i));
+		const auto nextMaxCost = maxPrice * minPeople;
+
+		if (nextMaxCost > maxCost)
+		{
+			totalMaxPrice = maxPrice;
+			maxCost = nextMaxCost;
+		}
+	}
+
+	cout << totalMaxPrice << ' ' << maxCost << endl;
+}

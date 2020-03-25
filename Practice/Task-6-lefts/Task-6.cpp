660330d7e13d34ad48b6b980cf819c1015b9457b
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-03-25 21:51:58 +0300

    добавлено 6ое праткическое задание

---
+++ Practice/Task-6-lefts/Task-6.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+
+struct Point {
+    int x, y;
+};
+
+struct Vector {
+    int x, y, z;
+
+    Vector operator*(Vector const &other) const;
+};
+
+Vector Vector::operator*(Vector const &other) const {
+    const auto cx = y * other.z - z * other.y;
+    const auto cy = z * other.x - x * other.z;
+    const auto cz = x * other.y - y * other.x;
+    return Vector{cx, cy, cz};
+}
+
+std::ostream &operator<<(std::ostream &out, Vector const &vector) {
+    out << vector.x << ' ' << vector.y << ' ' << vector.z;
+    return out;
+}
+
+std::istream &operator>>(std::istream &in, Point &point) {
+    in >> point.x >> point.y;
+    return in;
+}
+
+int main() {
+    try {
+        std::ifstream input("input.txt");
+        std::ofstream output("output.txt");
+
+        auto count = 0;
+        input >> count;
+
+        std::vector <Point> points;
+
+        while (count-- > 0) {
+            points.push_back(Point{});
+            input >> points.back();
+        }
+
+        std::vector <Vector> vectors;
+
+        for (auto i = 0; i < points.size() - 1; ++i) {
+            auto x = points[i + 1].x - points[i].x;
+            auto y = points[i + 1].y - points[i].y;
+            vectors.push_back({x, y, 0});
+        }
+
+        auto turns = 0;
+        auto turnsUp = 0;
+
+        for (std::size_t i = 0; i < vectors.size() - 1; ++i) {
+            const auto result = vectors[i] * vectors[i + 1];
+
+            if (result.z > 0) {
+                turns++;
+            }
+            if (result.z == 0) {
+                turnsUp++;
+            }
+        }
+
+        output << turns << " " << turnsUp << std::endl;
+    }
+    catch (std::exception &ex) {
+        std::cerr << ex.what() << std::endl;
+    }
+}
